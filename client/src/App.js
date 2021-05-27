fe3d032fa17a644a1e3eee2f1959e2acac93c7ad
ExceedPrince <tamas.bodorkos@hotmail.hu>
2021-05-27 15:28:06 +0200

    login & register system

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
@@ -6,13 +6,23 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
 
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
 
 const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <Provider store={store}>
       <Router>
