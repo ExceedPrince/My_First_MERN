0842634200c5be662297f63852cc2566b9beb056
ExceedPrince <tamas.bodorkos@hotmail.hu>
2021-05-30 15:09:20 +0200

    create profile last third

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { getCurrentProfile } from '../../actions/profile';
+import { getCurrentProfile, deleteAccount } from '../../actions/profile';
 
 import Spinner from '../layout/Spinner';
 import { Link } from 'react-router-dom';
 import DashboardActions from './DasboardActions';
+import Experience from './Experience';
+import Education from './Education';
 
 
-const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
+const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading }, deleteAccount }) => {
 	useEffect(() => {
 		getCurrentProfile()
 	}, [getCurrentProfile]);
@@ -22,6 +24,13 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
 			{profile !== null ?
 				(<>
 					<DashboardActions />
+					<Experience experience={profile.experience} />
+					<Education education={profile.education} />
+					<div className="my-2">
+						<button className="btn btn-danger" onClick={() => deleteAccount()}>
+							<i className="fas fa-user-minus"></i> Delete My Account
+						</button>
+					</div>
 				</>) :
 				(<>
 					<p>You have not yet setup a prfile, please add some info</p>
@@ -38,7 +47,8 @@ Dashboard.propTypes = {
 
 const mapStateToProps = (state) => ({
 	auth: state.auth,
-	profile: state.profile
+	profile: state.profile,
+	deleteAccount: PropTypes.func.isRequired,
 });
 
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
+export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(Dashboard);
