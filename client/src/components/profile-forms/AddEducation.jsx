a3342889f93696cabc376c69750ece0105305130
ExceedPrince <tamas.bodorkos@hotmail.hu>
2021-05-31 14:15:41 +0200

    something is wrong

--- client/src/components/profile-forms/AddEducation.jsx
+++ client/src/components/profile-forms/AddEducation.jsx
@@ -65,4 +65,4 @@ AddEducation.propTypes = {
 	addEducation: PropTypes.func.isRequired
 }
 
-export default connect(null, { addEducation })(AddEducation);
+export default connect(null, { addEducation })(withRouter(AddEducation));
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE } from '../actions/types';
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE, GET_PROFILES, GET_REPOS } from '../actions/types';
 
 const initialState = {
 	profile: null,
@@ -19,6 +19,8 @@ export default function (state = initialState, action) {
 				profile: payload,
 				loading: false
 			};
+		case GET_PROFILES:
+			return { ...state, profiles: payload, loading: false }
 		case PROFILE_ERROR:
 			return {
 				...state,
@@ -27,7 +29,9 @@ export default function (state = initialState, action) {
 				profile: null
 			};
 		case CLEAR_PROFILE:
-			return { ...state, profile: null, repos: [], loading: false }
+			return { ...state, profile: null, repos: [], loading: false };
+		case GET_REPOS:
+			return { ...state, repos: payload, loading: false }
 		default:
 			return state;
 	}
