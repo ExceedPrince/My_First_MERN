85c1b8d47fef18a431dbf336aaadde44f5983104
ExceedPrince <tamas.bodorkos@hotmail.hu>
2021-06-02 15:34:22 +0200

    g

--- server.js
+++ server.js
@@ -2,52 +2,30 @@ const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
 
-//Connect Database
+const app = express();
+
+// Connect Database
 connectDB();
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+// Init Middleware
+app.use(express.json());
 
-//Can the frontend do GET request
-app.use(function (req, res, next) {
-	// Website you wish to allow to connect
-	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-	// Request methods you wish to allow
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-	// Request headers you wish to allow
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-	// Set to true if you need the website to include cookies in the requests sent
-	// to the API (e.g. in case you use sessions)
-	res.setHeader('Access-Control-Allow-Credentials', true);
-	// Pass to next layer of middleware
-	next();
-});
-//Can post from frontend
-const cors = require('cors');
-const corsOptions = {
-	origin: 'http://localhost:3000',
-	credentials: true,            //access-control-allow-credentials:true
-	optionSuccessStatus: 200
-}
-app.use(cors(corsOptions));
-
-//Init middleware
-app.use(express.json({ extended: false }));
-
-//Define Routes
+// Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-//Serve staic assets in prduction
+// Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-	//set static folder
+	// Set static folder
 	app.use(express.static('client/build'));
 
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	})
+	});
 }
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
